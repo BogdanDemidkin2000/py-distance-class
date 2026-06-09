e86e110bb6da49cd9aed8ee3d6f8a4f2803c383d
Bogdan <bdemidkin@gmail.com>
2026-06-09 16:52:17 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: int) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,28 +12,28 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
 
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, int):
+        elif isinstance(other, (int | float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: Distance | int) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
 
         if isinstance(other, Distance):
             self.km += other.km
-        elif isinstance(other, int):
+        elif isinstance(other, (int | float)):
             self.km += other
         return self
 
-    def __mul__(self, other: int) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
 
         return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> Distance:
 
-        return Distance(round(self.km / other))
+        return Distance((round(self.km / other * 100)) / 100)
 
     def __lt__(self, other: int | float | Distance) -> bool:
 
